371be7be716c09a22039e412a78b10c84cfd0703
User <rushiprasad2004@gmail.com>
2026-02-20 14:15:25 +0530

    Initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import Portfolio from "./Portfolio";
+import SmokeCursor from "./WaveBackground.jsx";
+import SmokeBackground from "./WaveBackground.jsx";
+import WaveBackground from "./WaveBackground.jsx";
+
+function App() {
+    return (
+        <>
+            <WaveBackground />
+            <Portfolio />
+        </>
+    );
+}
+
+export default App;
